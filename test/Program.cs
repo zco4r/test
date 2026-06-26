f1362ca5ee099c5d3d268c0b0606cefa9b60da39
ammaralazki <alazkiyasser3@gmail.com>
2026-06-26 15:30:09 +0400

    users information

--- test/Program.cs
+++ test/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, CR7");
+﻿namespace test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please Enter Your Name:");
+        string userName = Console.ReadLine();
+            
+        Console.WriteLine("Please Enter Your Age:");
+        int userAge = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please Enter Your Salary:");
+        float usersalary = float.Parse(Console.ReadLine());
+        
+    }
+}
--- test/obj/Debug/net10.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net10.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d17652273096279ad91192ec0182f16f63a8f167")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
